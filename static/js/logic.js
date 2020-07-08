18c5e0c22e879552a63501f836f75d7941534274
Nehemiah B. Dang <nehemiahdang@gmail.com>
2020-07-07 18:12:08 -0700

    Updated Leaflet challenge

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,7 +2,7 @@
 var earthJson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
 
 // Create the map with a center point of California
-var myMap = L.map("map").setView([36.7783, -119.4179], 5);
+var myMap = L.map("map").setView([36.7783, -119.4179], 4);
 
 // Create the layer of the map
 var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -10,8 +10,9 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
     maxZoom: 18,
     id: "light-v10",
     accessToken: API_KEY
-  }).addTo(myMap);
+});
 
+// Create a function to generate a selected color based on the magnitude of the earthquake
 function chooseDisplay(mag) {
     if (mag <= 1) {
         color = "#36F011";
@@ -34,15 +35,18 @@ function chooseDisplay(mag) {
     return color;
 }
 
+// Create a function for a popup, giving the information on each earthquake point
 function onEachFeature (feature, layer) {
     layer.bindPopup("<h3><a target='_blank' href='" + feature.properties.url + "'>Click here</a><hr>" + feature.properties.place +
     "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
 }
 
+// Create a function to adjust the size of the markers
 function markerSize(mag) {
     return mag * 5;
 }
 
+// Create a function to get the wanted colors for the legend
 function getColor(value) {
     return value > 5 ? '#EC2F0E' :
         value > 4  ? '#F4950C' :
@@ -79,24 +83,46 @@ legend.onAdd = function() {
 legend.addTo(myMap);
 
 // Read the JSON file link
-d3.json(earthJson, function(data) {
+d3.json(earthJson, function(earthData) {
+    d3.json("static/data/PB2002_boundaries.json", function(boundData) {
     // console.log(data);
-    // console.log(data.features[0].geometry);
-    L.geoJson(data, {
-        onEachFeature: onEachFeature,
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: markerSize(feature.properties.mag),
-                fillColor: chooseDisplay(feature.properties.mag),
-                color: chooseDisplay(feature.properties.mag),
-                fillOpacity: 0.75,
-                Opacity: 0.75
-            });
-        },
-    }).addTo(myMap);
+        // Create the earthquakes map
+        var earthmap = L.geoJson(earthData, {
+            onEachFeature: onEachFeature,
+            pointToLayer: function (feature, latlng) {
+                // Change the markers to circles
+                return L.circleMarker(latlng, {
+                    radius: markerSize(feature.properties.mag),
+                    fillColor: chooseDisplay(feature.properties.mag),
+                    color: chooseDisplay(feature.properties.mag),
+                    fillOpacity: 0.75,
+                    Opacity: 0.75,
+                    stroke: false
+                });
+            },
+        });
+        
+        // Create the fault lines map
+        var faultmap = L.geoJson(boundData, {
+            style: function (feature) {
+                return {
+                    color: "orange"
+                }
+            }
+        });
+
+        // Create the base map layer option
+        var baseMaps = {
+            "Light": lightmap
+        };
+
+        // Create the overlay map options
+        var overlayMaps = {
+            "Earthquakes": earthmap,
+            "Fault Lines": faultmap
+        };
+
+        // Create the control option for the base and overlay maps
+        L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+    });
 });
-
-
-d3.json("static/data/PB2002_steps.json", function(data) {
-    console.log(data);
-})
